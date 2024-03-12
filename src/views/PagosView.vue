22cc9701d5c39d89c6e5ab7fbd7a60d31889177f
linamatiz <https://github.com/linamatiz/>
2024-03-11 19:32:01 -0500

    funcionalidades pagos

--- src/views/PagosView.vue
+++ src/views/PagosView.vue
@@ -1,35 +1,54 @@
 <template>
-    <div>
-      <h1> Pasarela de Pago</h1>
-      <div v-if="!paymentComplete">
-        <form @submit.prevent="processPayment">
-          <!-- Campos de entrada de la información del usuario -->
-          <div class="pagos">
-          <input type="text"  v-model="cardNumber" placeholder="Número de Tarjeta">
-          <br>
-          <input type="text"  v-model="expDate" placeholder="Fecha de Expiración">
-          <br>
-          <input type="text" v-model="cvv" placeholder="CVV">
-          <br>
-          <button class ="botonpagar" type="submit">Pagar</button>
+  <div>
+    <h1>Pasarela de Pago</h1>
+    <div v-if="!paymentComplete">
+      <form @submit.prevent="processPayment">
+        <!-- Campos de entrada de la información del usuario -->
+        <div class="pagos">
+          <div>
+            <label>Número de Tarjeta:</label>
+            <input type="text" v-model="cardNumber" placeholder="Número de Tarjeta">
           </div>
-        </form>
-      </div>
-      <div v-else>
-       <strong>listo tu pago, revisa tu correo .</strong>
+          <br>
+          <div>
+            <label>Fecha de Expiración:</label>
+            <input type="text" v-model="expDate" placeholder="Fecha de Expiración">
+          </div>
+          <br>
+          <div class="cvv">
+            <label>CVV:</label>
+            <input type="text" v-model="cvv" placeholder="CVV">
+          </div>
+          <button class="botonpagar  " type="submit">Pagar</button>
+        </div>
+      </form>
+
+      <!-- Sección para mostrar lo que se está escribiendo en tiempo real -->
+      <div class="datos">
+        <h2>Datos pago</h2>
+        <br>
+        <p>Número de Tarjeta: {{ cardNumber }}</p>
+        <br>
+        <p>Fecha de Expiración: {{ expDate }}</p>
+        <br>
+        <p>CVV: {{ cvv }}</p>
       </div>
     </div>
-  </template>
-  
-  <script>
-  import Swal from 'sweetalert2';
+    <div v-else>
+      <strong>listo tu pago, revisa tu correo.</strong>
+    </div>
+  </div>
+</template>
 
-  export default {
+<script>
+import Swal from 'sweetalert2';
+
+export default {
   data() {
     return {
       cardNumber: '',
       expDate: '',
-      cvc: '',
+      cvv: '',
       paymentComplete: false
     };
   },
@@ -42,49 +61,40 @@
           title: '¡pago exitoso!',
           text: 'tu pago fue completado',
           icon: 'success',
-        confirmButtonText: 'Entendido',
-        confirmButtonColor: '#43d5e8',
-      });
+          confirmButtonText: 'Entendido',
+          confirmButtonColor: '#43d5e8',
+        });
       }, 300);
     }
   }
 };
-
-
 </script>
 
-  
-  <style scoped>
-  /* Estilos CSS para la vista */
-  
-  .paymentcomplete{
-    background-color: #ffffff;
+<style scoped>
+/* Estilos CSS para la vista */
+.cvv{
+ margin-left: 70px;
+}
+.pagos {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
   padding: 20px;
-  border-radius: 5px;
-  text-align: center;
-  box-shadow: 0 0 10px rgba(0, 0, 0, 0.302);
+  background-color: #a0dfdf;
+}
 
-  }
-  .pagos
-  {
-    
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    width: 100%;
-    height: 100%;
-    background-color: #637878;
-    padding: 20px;
-  }
-  .botonpagar{
-    border-radius: 8px; /* Bordes redondeados */
-    background-color: #43d5e8; /* Cambia el color de fondo al pasar el cursor */
-  }
+.botonpagar {
+  margin-top: 35px;
+  border-radius: 13px;
+  background-color: #43d5e8;
+  cursor: pointer;
+}
 
-  .botonconfirmacion {
-    border-radius: 45px !important; /* Bordes redondeados */
-    background-color: rgb(110, 171, 177) !important; /* color */
-  }
-
-  </style>
-  
+.datos {
+  margin-top: 20px;
+  background-color: #e4e3e358;
+  padding: 10px;
+  border-radius: 10px;
+}
+</style>
